e10038bca0e88d05ad5078aab411b33dfd21433e
BrunoBarrosR <bruno_barros@hotmail.com.br>
2023-09-03 20:14:27 -0300

    Update script.js

--- aula14ex/exerciciojs4/script.js
+++ aula14ex/exerciciojs4/script.js
@@ -11,12 +11,20 @@ function contar() {
         var i = Number(ini.value)
         var f = Number(fim.value)
         var p = Number(passo.value)
-
-        for (var c = i; c <= f; c += p) {
-            res.innerHTML += `${c} `
+        if (i < f) {
+            //contagem crescente
+            for (var c = i; c <= f; c += p) {
+                res.innerHTML += `${c} \u{1F449}`
+            }
+            
+        } else {
+            //contagem decrescente
+            for(var c = i; c >= f; c -= p){
+                res.innerHTML += `${c} \u{1F449}`
+            }
         }
+        res.innerHTML += '\u{1F3C1}'
     }
+
 }
-
-
-
+// 	U+1F607 Código no site https://unicode.org/emoji/charts/full-emoji-list.html
